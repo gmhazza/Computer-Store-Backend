c3b173884f2cb2be8fb79020b68bc9a5a8f5dc3a
Muhammad Hazza Shabir <mhazzashabir.com>
2026-07-10 04:25:05 +0500

    supabase product selection v2

--- gemini/index.js
+++ gemini/index.js
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({ 
   model: 'gemini-2.5-flash',
-  systemInstruction: "You will help my customers to buy computer parts which are available in my store. You will answer their questions based on the available products. If the customer asks for a product that is not available, you will politely inform them that the product is not available and suggest alternative products if possible.",
+  systemInstruction: "You will help my customers to buy computer parts which are available in my store. You will answer their questions based on the available products. If the customer asks for a product that is not available, you will politely inform them that the product is not available and suggest alternative products if possible. and also try to give short answers.",
 });
 
 module.exports = model;
--- index.js
+++ index.js
@@ -11,10 +11,26 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+
+
+app.get('/login', async (req, res) => {
+    const { error: authError } = await supabase.auth.signInWithPassword({
+    email: process.env.SUPABASE_EMAIL,
+    password: process.env.SUPABASE_PASSWORD,
+    });
+    console.log(authError);
+    if (authError) {
+        console.error('Error signing in to Supabase:', authError);
+        process.exit(1);
+    }
+});
+
 app.post('/gemini', async (req, res) => {
     const prompt = req.body
     try {
         const { data, error } = await supabase.from('products').select('*')
+        console.log('data: ', data)
+        console.log('error: ', error)
         prompt.prompt += '\n I am also providing you available products. Answer the question based on the available products if necessary. Here are the available products: ' + JSON.stringify(data)
     } catch (error) {
         console.error('Error fetching products from Supabase:', error);
