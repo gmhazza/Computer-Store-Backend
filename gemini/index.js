e114043a4fb1d279ea88fbebe7a92c0a858a50c6
Muhammad Hazza Shabir <mhazzashabir.com>
2026-07-10 02:58:10 +0500

    supabase product selection

--- gemini/index.js
+++ gemini/index.js
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = genAI.getGenerativeModel({ 
   model: 'gemini-2.5-flash',
-  systemInstruction: "You are a helpful and concise backend assistant.",
+  systemInstruction: "You will help my customers to buy computer parts which are available in my store. You will answer their questions based on the available products. If the customer asks for a product that is not available, you will politely inform them that the product is not available and suggest alternative products if possible.",
 });
 
 module.exports = model;
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const generateResponse = require('./responceGen.js')
 require('dotenv').config()
 
+const supabase = require('./database/supabase.js')
+
 const port = process.env.PORT || 3000
 
 const app = express()
@@ -11,6 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/gemini', async (req, res) => {
     const prompt = req.body
+    try {
+        const { data, error } = await supabase.from('products').select('*')
+        prompt.prompt += '\n I am also providing you available products. Answer the question based on the available products if necessary. Here are the available products: ' + JSON.stringify(data)
+    } catch (error) {
+        console.error('Error fetching products from Supabase:', error);
+        res.status(500).json({ success: false, message: 'Error fetching products from Supabase' });
+        return;
+    }
+
     const generatedResponse = await generateResponse(prompt.prompt)
     res.json(generatedResponse)
     
